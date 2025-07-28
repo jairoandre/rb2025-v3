87d7b3af6c1ba409f1aba732cd593c20d3bcf543
jairoandre <jairo.andre@gmail.com>
2025-07-27 21:02:33 -0300

    remove init sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE UNLOGGED TABLE IF NOT EXISTS payments (
-    correlation_id UUID PRIMARY KEY,
-    amount DECIMAL(10, 2) NOT NULL,
-    requested_at TIMESTAMPTZ NOT NULL,
-    processor SMALLINT NOT NULL
-);
-
-CREATE INDEX CONCURRENTLY idx_processor_requested_at ON payments(requested_at, processor);
